dc03cbaf4ab232c86bb4367b2d54f8a8f720ac24
Anurag Jain <ajsanuragjain@gmail.com>
2017-03-20 22:11:19 +0530

    remove temp files

--- storage/framework/views/56e5fb00f3a1beba57a16436e91fa2a1745e48d1.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection("content"); ?>
-    <h1>Page Not Found</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make("public.base", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
